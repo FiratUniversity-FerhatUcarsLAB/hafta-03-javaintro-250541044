fdc1c3b6ae68b71dbc40acbb85f0ea76bb2860ec
250541044 <250541044@firat.edu.tr>
2025-10-23 15:46:28 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,19 @@
+public class ASCIIArt {
+
+    public static void main(String[] args) {
+
+
+        String e_ascii_art = """
+              EEEEEEEEE
+              EE
+              EE
+              EEEEEE
+              EE
+              EE
+              EEEEEEEEE
+            """;
+
+
+        System.out.println(e_ascii_art);
+    }
+}
