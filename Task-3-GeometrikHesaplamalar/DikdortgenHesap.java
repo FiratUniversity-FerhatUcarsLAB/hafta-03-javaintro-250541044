e8293372413275de42b39da4b7e0349cdae79495
250541044 <250541044@firat.edu.tr>
2025-10-23 15:45:21 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,11 @@
+public class DikdortgenHesap {
+     static void main(String[] args) {
+        double en =  4.5;
+        double boy = 7.9;
+        double cevre = 2*boy+2*en;
+        double alan = boy*en;
+        System.out.printf("%.2f\n",alan);
+        System.out.printf("%.2f\n",cevre);
+
+    }
+}
