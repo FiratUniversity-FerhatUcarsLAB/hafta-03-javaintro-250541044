4bee80cf2c22dccb831cc7c7246d926c7ba563b8
250541044 <250541044@firat.edu.tr>
2025-10-23 15:47:35 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class IsimKarti {
+    static void main(String[] args) {
+        String ad = " Eren Bilge ";
+        String soyad = " Koçak ";
+        String ogrNo = " 250541044";
+        String bolum = " Yazilim Muh .";
+
+        System.out.println(" =================== ") ;
+        System.out.println(" OGRENCI BILGILERI ") ;
+        System.out.println(" =================== ") ;
+        System.out.println("Ad : " + ad ) ;
+        System.out.println(" Soyad : " + soyad ) ;
+        System.out.println("No : " + ogrNo ) ;
+        System.out.println(" Bolum : " + bolum ) ;
+        System.out.println(" =================== ") ;
+
+    }
+}
