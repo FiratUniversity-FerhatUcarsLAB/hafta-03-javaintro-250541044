3d586dd722b5badcaad41b45d6a78cadd008addb
250541044 <250541044@firat.edu.tr>
2025-10-23 15:37:10 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    static void main(String[] args) {
+    System.out.println("Java'ya Hoş Geldniz");
+    System.out.println("Bilgisayar Bilimlerine Hoşgeldiniz");
+    System.out.println("Programlama Eğlencelidir");
+    }
+}
